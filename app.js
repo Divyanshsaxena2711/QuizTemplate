dcaf1bd5f0f4b9c737b6e4a38dd45fa20989693e
divyansh <itsdivyanshsaxena@gmail.com>
2022-12-09 05:50:45 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.listen(3000,function(){
+
+    console.log("server started on port 3000");
+})
